eedd52b7df6bf51b47c487c305fe69a89fcd51d6
Luigi Minardi <luigiminardim@gmail.com>
2021-05-11 09:24:04 -0300

    Variables names.

--- r2a/r2a_bola.py
+++ r2a/r2a_bola.py
@@ -1,4 +1,3 @@
-from r2a.ir2a import IR2A
 import math
 from player.parser import *
 import time
@@ -9,73 +8,80 @@ from base.configuration_parser import ConfigurationParser
 
 class R2A_Bola(IR2A):
 
-    _qualities = []
+    # S_m / p = S_m / 1 = S_m
+    S = []
 
-    _smoothness_weight = 0.0  # (\gama)
+    g = 0.0  # \gamma
 
-    _performance_weight = 0.0  # (V)
+    V = 0.0
 
     def __init__(self, id):
         IR2A.__init__(self, id)
 
-    def _utility(self,  quality):
-        return math.log(quality / min(self._qualities))
+    def v(self,  m):
+        '''
+        Marginal utility
+        v_m = ln(S_m / S_0)
+        '''
+        return math.log(self.S[m] / self.S[0])
 
-    def _trend_to_download(self, quality, buffer_size):
-        return (
-            self._performance_weight * self._utility(quality)
-            + self._performance_weight * self._smoothness_weight
-            - buffer_size
-        ) / quality
+    def t(self, m):
+        '''
+        Tendency
+        t_m = (V * (v_m + \gamma * p) - Q) / (S_m)
+        '''
+        Q = Whiteboard.get_instance().get_amount_video_to_play()
+        return (self.V * (self.v(m) + self.g * 1) - Q) / self.S[m]
 
-    def _select_quality(self, buffer_size):
-        selected_quality = None
-        max_trend = -math.inf
-        for quality in self._qualities:
-            quality_trend = self._trend_to_download(quality, buffer_size)
-            if quality_trend >= max_trend:
-                selected_quality = quality
-                max_trend = quality_trend
-        return selected_quality
+    def _select_quality_index(self):
+        '''
+        m that maximizes t(m)
+        '''
+        m_selected = None
+        t_max = -math.inf
+        for m in range(0, len(self.S) - 1):
+            t = self.t(m)
+            if t >= t_max:
+                m_selected = m
+                t_max = t
+        return m_selected
+
+    def initialize(self):
+        pass
 
     def handle_xml_request(self, msg):
         self.send_down(msg)
 
     def handle_xml_response(self, msg):
+        '''
+        V = (Q_max - 1) / (v_max + \gamma  * p)
+        \gamma = ((Q_max - 1) * (v_0 * S_1 - v_1 * S_0) - 2 * v_max * (S_1 - S_0)) / (p * (S_1 - S_0) * (2 - (Q_max - 1)))
+        '''
         parsed_mpd = parse_mpd(msg.get_payload())
-        self._qualities = parsed_mpd.get_qi()
-        max_buffer_size = ConfigurationParser.get_instance().get_parameter('max_buffer_size')
-        min_quality = self._qualities[0]
-        snd_quality = self._qualities[1]
-        max_quality = self._qualities[len(self._qualities) - 1]
-        self._smoothness_weight = (
-            (max_buffer_size - 1)
-            * (self._utility(min_quality) * snd_quality - self._utility(snd_quality) * min_quality)
-            - 2 * self._utility(max_quality) * (snd_quality - min_quality)
+        self.S = parsed_mpd.get_qi()
+        Q_max = ConfigurationParser.get_instance().get_parameter('max_buffer_size')
+        v_max = self.v(len(self.S) - 1)
+        self.g = (
+            (Q_max - 1) * (self.v(0) * self.S[1] - self.v(1) * self.S[0])
+            - 2 * v_max * (self.S[1] - self.S[0])
         ) / (
-            (snd_quality - min_quality) * (2 - max_buffer_size)
+            1 * (self.S[1] - self.S[0]) * (2 - Q_max - 1)
         )
-        self._performance_weight = (
-            max_buffer_size - 1) / (self._utility(max_quality) + self._smoothness_weight)
+        self.V = (Q_max - 1) / (v_max + self.g * 1)
         self.send_up(msg)
-        print('Qualities: ', self._qualities)
-        print('Min quality: ', min_quality)
-        print('Second quality: ', snd_quality)
-        print('Max quality: ', max_quality)
-        print('Smoothness weight: ', self._smoothness_weight)
-        print('Performance weight: ', self._performance_weight)
+        print('Qualities: ', self.S)
+        print('Smoothness weight: ', self.g)
+        print('Performance weight: ', self.V)
+        print('Q_max', Q_max)
+        print('v_max', v_max)
 
     def handle_segment_size_request(self, msg):
-        buffer_size = Whiteboard.get_instance().get_amount_video_to_play()
-        selected_quality = self._select_quality(buffer_size)
-        msg.add_quality_id(selected_quality)
+        m = self._select_quality_index()
+        msg.add_quality_id(self.S[m])
         self.send_down(msg)
 
     def handle_segment_size_response(self, msg):
         self.send_up(msg)
-
-    def initialize(self):
-        pass
 
     def finalization(self):
         pass
